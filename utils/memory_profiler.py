e3ddc37b663bb788ae1e37bfdd4e2a834ebfbd08
felix <felix.helfrich@gmail.com>
2019-03-12 12:32:49 +0100

    added class to track memory usage of process

---
+++ utils/memory_profiler.py
@@ -0,0 +1,34 @@
+import subprocess
+import memory_profiler
+
+class MemoryProfiler:
+
+    def profile(self, args, interval = 0.1):
+        with subprocess.Popen(args) as process:
+            memory_usage = memory_profiler.memory_usage(process, interval=interval)
+            print(memory_usage)
+            peak_memory_usage = max(memory_usage)
+            return MemoryUsageInfo(memory_usage, peak_memory_usage, interval)
+
+class MemoryUsageInfo:
+
+    def __init__(self, memory_usage, peak_memory_usage, interval):
+        self.memory_usage = memory_usage
+        self.peak_memory_usage = peak_memory_usage
+        self.interval = interval
+
+    def __format_data_point(self, data_point):
+        if data_point > 1024:
+            value = data_point/1024
+            unit = "Gb"
+        else:
+            value = data_point
+            unit = "Mb"
+        return "{} {}".format(round(value, 3), unit)
+
+    def __str__(self):
+        str_parts = [ "Peak usage: {}".format(self.__format_data_point(self.peak_memory_usage)) ]
+        for data_point in self.memory_usage:
+            str_parts.append(self.__format_data_point(data_point))
+        return "\n".join(str_parts)
+
