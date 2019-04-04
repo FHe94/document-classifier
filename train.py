db1ee9d3c6f253afb75911ff30ba11bf6e8af348
felix <felix.helfrich@gmail.com>
2019-04-04 11:30:04 +0200

    Add command line tool to enter training parameters
    This commit adds an alternative to using a config file for training and
    allows to enter paramters using an interactive command line tool. For
    now this only allows to train a single model per training session.

--- train.py
+++ train.py
@@ -3,19 +3,25 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 from classifier.loading.training_config_parser import TrainingConfigParser
 from experiment import Experiment
 import argparse
+import utils.interactive_config_reader as config_reader
 
 
 def main():
     args = parse_args()
-    training_config = TrainingConfigParser().parse_config(args.training_config_path)
+    training_config = get_training_config(args)
     train_experiment = Experiment(args.experiment_dir, training_config)
     train_experiment.run_train()
 
+def get_training_config(args):
+    if args.config == "":
+        return config_reader.IneractiveConfigReader().read_training_config()
+    else:
+        return TrainingConfigParser().parse_config(args.training_config_path)
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("experiment_dir")
-    parser.add_argument("training_config_path")
+    parser.add_argument("-c", "--config", metavar = "training_config_path", default = "")
     return parser.parse_args()
 
 
---
+++ utils/interactive_config_reader.py
@@ -0,0 +1,25 @@
+from classifier.loading.training_config_parser import TrainingConfig, ModelTrainingConfig
+
+class IneractiveConfigReader:
+
+    def read_training_config(self):
+        dataset_dir = self.__get_mandatory_input("Dataset directory")
+        num_epochs = self.__get_input_or_default("Number of epochs", 50, int)
+        model_architecture = self.__get_input_or_default("Model architecture", "cnn")
+        model_name = self.__get_input_or_default("Model name", model_architecture)
+        document_processor = self.__get_input_or_default("Document processor", "default")
+        return TrainingConfig(num_epochs, dataset_dir, [ ModelTrainingConfig(model_name, model_architecture, document_processor, dict()) ])
+
+    def __get_input_or_default(self, prompt, default, type = str):
+        user_input = input("{} ({}):".format(prompt, str(default)))
+        if len(user_input) == 0:
+            user_input = default
+        return type(user_input)
+
+    def __get_mandatory_input(self, prompt, type = str):
+        formatted_prompt = "{}:".format(prompt)
+        user_input = input(formatted_prompt)
+        while user_input == "":
+            print("Input is required and cannot be empty")
+            user_input = input(formatted_prompt)
+        return type(user_input)
